71417fcb9662f7c0ec6c90a9510b3565074beff8
Administrator <yangningxiao123@163.com>
2019-01-07 23:27:25 +0800

    新增记录播放位置

--- src/main/java/com/yangdaodao/demopdfaudio/controller/AssistController.java
+++ src/main/java/com/yangdaodao/demopdfaudio/controller/AssistController.java
@@ -3,9 +3,12 @@ package com.yangdaodao.demopdfaudio.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.yangdaodao.demopdfaudio.util.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.springframework.boot.system.ApplicationHome;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,13 +22,15 @@ import static org.apache.tomcat.util.compat.JrePlatform.IS_WINDOWS;
 
 @Controller
 @RequestMapping("assist")
+@Slf4j
 public class AssistController {
 
 	public String jarParentPath = new ApplicationHome(getClass()).getSource().getParentFile().toString();
 	public String outerStaticPath = !IS_WINDOWS ? (jarParentPath + "/outer-static") : "F:/BaiduNetdiskDownload/outer-static";
 
-	@RequestMapping({"init", "test"})
-	public String init() {
+	@RequestMapping("init")
+	public String init(String confName, ModelMap modelMap) {
+		modelMap.addAttribute("confName", confName);
 		return "init";
 	}
 
@@ -48,10 +53,10 @@ public class AssistController {
 		return "/" + obsKey;
 	}
 
-	@RequestMapping("loadUserConf")
+	@RequestMapping("loadUserConf/{confName}")
 	@ResponseBody
-	public JSONObject loadUserConf(String confName) throws IOException {
-		File confFile = new File(outerStaticPath + "/conf.json");
+	public JSONObject loadUserConf(@PathVariable String confName) throws IOException {
+		File confFile = new File(outerStaticPath + "/conf-" + confName + ".json");
 		JSONObject conf = confFile.exists() ? JSONObject.parseObject(FileUtils.readFileToString(confFile, "utf-8")) : new JSONObject();
 		if (!confFile.exists()) {
 			FileUtils.write(confFile, JSON.toJSONString(conf, true), "utf-8");
@@ -59,10 +64,10 @@ public class AssistController {
 		return conf;
 	}
 
-	@RequestMapping("updateUserConf")
+	@RequestMapping("updateUserConf/{confName}")
 	@ResponseBody
-	public void updateUserConf(@RequestBody JSONObject userConf) throws IOException {
-		File confFile = new File(outerStaticPath + "/conf.json");
+	public void updateUserConf(@RequestBody JSONObject userConf, @PathVariable String confName) throws IOException {
+		File confFile = new File(outerStaticPath + "/conf-" + confName + ".json");
 		FileUtils.write(confFile, JSON.toJSONString(userConf, true), "utf-8");
 	}
 
